748c9d582ea49a73f16ce981d8c8ac1a4341e293
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-01-27 02:56:02 +0600

    GBN BAIC code with BUGS

--- Reliable Transport Protocol/Offline/script.sh
+++ Reliable Transport Protocol/Offline/script.sh
@@ -1,6 +1,10 @@
 clear
-gcc rdt_abp.c
-./a.out > output_abp.doc
+# gcc rdt_abp.c
+# ./a.out > output_abp.doc
+# cat output_abp.doc
+
+g++ rdt_gbn.cpp
+./a.out > output_gbn.doc
 # ./a.out
-cat output_abp.doc
+cat output_gbn.doc
 rm a.out
